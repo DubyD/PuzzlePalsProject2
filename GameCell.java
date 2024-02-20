8052202cb8fa909993c9d88d3e2311bd7e88c744
DubyD <140568071+DubyD@users.noreply.github.com>
2024-02-20 10:30:37 -0500

    Updating moving parts
    Allowing the GameCell to have a pointer to the parent PuzzleTable to allow for updating the the children in corresponding columns and rows

--- GameCell.java
+++ GameCell.java
@@ -1,21 +1,30 @@
 //Author: WD
-
-import javafx.scene.layout.GridPane;
+//WD---vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 import javafx.scene.control.Label;
 
 
 public class GameCell {
-//WD---vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
     private String cellState;
     private Label pieceLabel;
     private boolean isHeader;
-    public GameCell(){
+    private PuzzleTable puzzleSection;
+    private int row;
+    private int column;
+    public GameCell(PuzzleTable parent, int row, int column){
 
             //Allows easy access to which state the cell is currently in while switching
             //through it's cycle of states
         this.cellState = "";
         this.pieceLabel = new Label();
         this.isHeader = false;
+
+            //For reference to adjust the objects in other grid spaces
+        this.puzzleSection = parent;
+
+            //To identify which spaces need to be adjusted
+        this.row = row;
+        this.column = column;
 
             //Creates a rotating state of values to play the game with.
         this.pieceLabel.setOnMouseClicked(event -> {
@@ -25,6 +34,27 @@ public class GameCell {
             }else if(this.cellState.equals("X")){
                 this.cellState = "O";
                 this.pieceLabel.setText(this.cellState);
+                GameCell[][] updating = this.puzzleSection.getSection();
+
+                    //Updates the Board if 'O' is selected to 'X' out all non-Header Column and rows of the same value
+                for(GameCell[] layer: updating) {
+                    for (GameCell working : layer) {
+                        if (working.getRow() == this.row && working.getColumn() == this.column) {
+                            //nothing happens because that is this. GameCell
+
+                        } else if (working.getRow() == this.row) {
+                            //'X' in the Row
+                            working.setLabel("X");
+                        } else if (working.getColumn() == this.column) {
+                            //'X' in the Column
+                            working.setLabel("X");
+                        }
+                    }
+                }
+                    //Updates the Board to fill in 'X's into rows and columns
+                this.puzzleSection.updatePuzzle(updating, this.puzzleSection.getRowHeader(), this.puzzleSection.getColumnHeader());
+
+
             }else if(this.cellState.equals("O")){
                 this.cellState = "";
                 this.pieceLabel.setText(this.cellState);
@@ -43,13 +73,25 @@ public class GameCell {
         this.pieceLabel = new Label(header);
     }
 
-        //Gives higher level classes access to what kind of object this is easier
+        //Used to update PuzzleSection
+    public int getRow(){
+        return this.row;
+    }
+        //Used to update PuzzleSection
+    public int getColumn(){
+        return this.column;
+    }
+
+        //Gives access to what kind of object this is, making updates easier
     public boolean getCellType(){
         return this.isHeader;
     }
-        //Allows the object that was created to be sent into a larger class without the need to set the constructor
-    public Label getLabel(){
-        return this.pieceLabel;
+        //Allows the GameCell to be updated by the click events of other GameCells
+    public void setLabel(String update){
+            //checks the CellType so that Headers keep their values
+        if(this.getCellType() == false) {
+            this.pieceLabel.setText(update);
+        }
     }
 
 
--- PuzzleTable.java
+++ PuzzleTable.java
@@ -1,9 +1,43 @@
+//Author: WD
+//WD---vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+import javafx.scene.control.TableView;
+
 public class PuzzleTable {
     private GameCell[][] puzzlePiece;
+    private TableView<GameCell> puzzleSection;
+    private boolean hasRowHeaders;
+    private boolean hasColumnHeaders;
 
-    public PuzzleTable(int rows, int columns){
+    public PuzzleTable(int rows, int columns, boolean hasRowHeaders, boolean hasColumnHeaders){
 
         this.puzzlePiece = new GameCell[rows][columns];
+        this.puzzleSection = new TableView<>();
+
+            //For formatting the game to include A row or column of un-clickable Labels
+        this.hasRowHeaders = hasRowHeaders;
+        this.hasColumnHeaders = hasColumnHeaders;
 
     }
+
+    public void setFirstPuzzle(List<String> columnHeader, List<String> rowHeader){
+
+    }
+    public GameCell[][] getSection(){
+        return this.puzzlePiece;
+    }
+
+        //Used for Creating and updating the Puzzle
+    public void updatePuzzle(GameCell[][] update, boolean hasColumnHeaders, boolean hasRowHeaders){
+
+    }
+
+        //Used for updating the Puzzle
+    public boolean getRowHeader(){
+        this.hasRowHeaders;
+    }
+        //Used for updating the Puzzle
+    public boolean getColumnHeader(){
+        this.hasColumnHeaders;
+    }
+//WD--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
