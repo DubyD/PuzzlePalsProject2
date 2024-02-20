d7563cb640c553ded5e1c924fe19740b5ebe9fe9
DubyD <140568071+DubyD@users.noreply.github.com>
2024-02-20 09:43:28 -0500

    Update GameCell.java
    Added Methods to use for higher level classes

--- GameCell.java
+++ GameCell.java
@@ -1,27 +1,58 @@
 //Author: WD
 
-import javafx.scene.control.Cell;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
 
 
 public class GameCell {
-//WD---vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+//WD---vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     private String cellState;
+    private Label pieceLabel;
     private boolean isHeader;
-    private Cell gridPiece;
-    public GameCell(boolean header){
+    public GameCell(){
 
             //Allows easy access to which state the cell is currently in while switching
             //through it's cycle of states
         this.cellState = "";
-            //Determines whether this is a label cell or a
-        this.isHeader = header;
+        this.pieceLabel = new Label();
+        this.isHeader = false;
 
-        if(header == false){
-
-        }
-
-
+            //Creates a rotating state of values to play the game with.
+        this.pieceLabel.setOnMouseClicked(event -> {
+            if(this.cellState.equals("")){
+                this.cellState = "X";
+                this.pieceLabel.setText(this.cellState);
+            }else if(this.cellState.equals("X")){
+                this.cellState = "O";
+                this.pieceLabel.setText(this.cellState);
+            }else if(this.cellState.equals("O")){
+                this.cellState = "";
+                this.pieceLabel.setText(this.cellState);
+            }
+        });
     }
-//WD--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+        //The second constructor is for headers of rows and columns, separating them
+        //from the game pieces
+    public GameCell(String header){
+
+            //This will make it easier to identify clickable from
+            // un-clickable cells from the backend
+        this.isHeader = true;
+        this.cellState = "";
+        this.pieceLabel = new Label(header);
+    }
+
+        //Gives higher level classes access to what kind of object this is easier
+    public boolean getCellType(){
+        return this.isHeader;
+    }
+        //Allows the object that was created to be sent into a larger class without the need to set the constructor
+    public Label getLabel(){
+        return this.pieceLabel;
+    }
+
+
+//WD--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 }
