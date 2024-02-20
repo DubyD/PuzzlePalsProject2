bb81832343433c927227429e44504452b622fc6e
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-02-20 09:14:16 -0500

    + temp file path, static function, scanner

--- PuzzleReader.java
+++ PuzzleReader.java
@@ -1,2 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
 public class PuzzleReader {
+    private String csvFilePath;
+    private File csvFile;
+    private Scanner csvScanner;
+    public PuzzleReader(){
+        csvFilePath="";
+        csvFile= new File(csvFilePath);
+        csvScanner=new Scanner(csvFile);
+    }
+    public static Puzzle readCSV(puzInput){
+
+    }
 }
