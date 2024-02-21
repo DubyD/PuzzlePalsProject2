7b29fd4149477bd648bc7b22dc61a724978843ed
Eevie:) <eevie1421@gmail.com>
2024-02-21 16:33:23 -0500

    Changes to GameCell Added Constructor + started to lay framework for Puzzle

--- GameCell.java
+++ GameCell.java
@@ -1,109 +1,30 @@
 //Author: WD
 //WD---vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-import javafx.scene.control.Label;
+
 
 
 public class GameCell {
+    private int curVal;
 
-    private String cellState;
-    private Label pieceLabel;
-    private boolean isHeader;
-    private PuzzleTable puzzleSection;
-    private int row;
-    private int column;
-    public GameCell(PuzzleTable parent, int row, int column){
-
-            //Allows easy access to which state the cell is currently in while switching
-            //through it's cycle of states
-        this.cellState = "";
-        this.pieceLabel = new Label();
-        this.isHeader = false;
-
-            //For reference to adjust the objects in other grid spaces
-        this.puzzleSection = parent;
-
-            //To identify which spaces need to be adjusted
-        this.row = row;
-        this.column = column;
-
-            //Creates a rotating state of values to play the game with.
-        this.pieceLabel.setOnMouseClicked(event -> {
-            if(this.cellState.equals("")){
-                this.cellState = "X";
-                this.pieceLabel.setText(this.cellState);
-            }else if(this.cellState.equals("X")){
-                this.cellState = "O";
-                this.pieceLabel.setText(this.cellState);
-                GameCell[][] updating = this.puzzleSection.getSection();
-
-                    //Updates the Board if 'O' is selected to 'X' out all non-Header Column and rows of the same value
-                for(GameCell[] layer: updating) {
-                    for (GameCell working : layer) {
-                        if (working.getRow() == this.row && working.getColumn() == this.column) {
-                            //nothing happens because that is this. GameCell
-
-                        } else if (working.getRow() == this.row) {
-                            //'X' in the Row
-                            working.setLabel("X");
-                        } else if (working.getColumn() == this.column) {
-                            //'X' in the Column
-                            working.setLabel("X");
-                        }
-                    }
-                }
-                    //Updates the Board to fill in 'X's into rows and columns
-                this.puzzleSection.updatePuzzle(updating, this.puzzleSection.getRowHeader(), this.puzzleSection.getColumnHeader());
-
-
-            }else if(this.cellState.equals("O")){
-                this.cellState = "";
-                this.pieceLabel.setText(this.cellState);
-            }
-        });
+    public GameCell(){
+        curVal = 0;
     }
 
-        //The second constructor is for headers of rows and columns, separating them
-        //from the game pieces
-    public GameCell(String header){
-
-            //This will make it easier to identify clickable from
-            // un-clickable cells from the backend
-        this.isHeader = true;
-        this.cellState = "";
-        this.pieceLabel = new Label(header);
+    public int getCurVal() {
+        return curVal;
     }
 
-        //Used to update PuzzleSection
-    public int getRow(){
-        return this.row;
-    }
-        //Used to update PuzzleSection
-    public int getColumn(){
-        return this.column;
+    public void setCurVal(int curVal) {
+        this.curVal = curVal;
     }
 
-        //Gives access to what kind of object this is, making updates easier
-    public boolean getCellType(){
-        return this.isHeader;
-    }
-        //Allows the GameCell to be updated by the click events of other GameCells
-    public void setLabel(String update){
-            //checks the CellType so that Headers keep their values
-        if(this.getCellType() == false) {
-            this.pieceLabel.setText(update);
+    public int clickCell(){
+        if(curVal < 2){
+            curVal++;
         }
-    }
-
-        //Used in Puzzle Table
-    public void setHeader(String header){
-
-            //Used to set Headers of a Puzzle
-        if(this.getCellType() == true){
-            this.pieceLabel.setText(header);
+        else {
+            curVal = 0;
         }
+        return curVal;
     }
-
-
-//WD--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
 }
--- Puzzle.java
+++ Puzzle.java
@@ -1,2 +1,72 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Puzzle {
+    private ArrayList<GameCell[][]> gBoard;
+    private LinkedList<ArrayList<GameCell[][]>> stateStack;
+    private ArrayList<GameCell[][]> answerKey;
+
+    /**
+     * initializes board with given size.
+     * @param categories - number of categories
+     * @param items - number of items in each category
+     * @param answerKey - answerkey
+     */
+    public Puzzle(int categories, int items, ArrayList<GameCell[][]> answerKey){
+        gBoard = new ArrayList<>();
+        this.answerKey = answerKey;
+        stateStack = new LinkedList<>();
+        int sqrNum;
+        if(categories == 3){
+            sqrNum = 3;
+        }
+        else {
+            sqrNum = 6;
+        }
+        for(int i = 0; i < sqrNum; i++){
+            gBoard.add(new GameCell[items][items]);
+            for(int j = 0; j < items; j++){
+                for (int k = 0; k < items; k++){
+                    gBoard.get(i)[j][k] = new GameCell();
+                }
+            }
+        }
+        stateStack.push(gBoard);
+    }
+
+    public void setAnswerKey(ArrayList<GameCell[][]> answerKey) {
+        this.answerKey = answerKey;
+    }
+
+    public void clickCell(int square, int xval, int yval){
+        int val = gBoard.get(square)[xval][yval].clickCell();
+        if(val == 2){
+            for(int i = 0; i < gBoard.get(square).length; i++){
+                if(i != xval){
+                    gBoard.get(square)[i][yval].setCurVal(1);
+                }
+                if (i != yval){
+                    gBoard.get(square)[xval][i].setCurVal(1);
+                }
+            }
+        }
+        stateStack.push(gBoard);
+    }
+
+    public boolean clearErrors(){
+        return true;
+    }
+
+    public ArrayList<GameCell[][]> getgBoard(){
+        return gBoard;
+    }
+
+    public void undo(){
+
+    }
+
+    public void hint(){
+
+    }
+
 }
