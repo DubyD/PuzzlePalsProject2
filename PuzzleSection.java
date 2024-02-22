22dcf7562207ddb7858e698230c761a1f0250345
DubyD <140568071+DubyD@users.noreply.github.com>
2024-02-22 11:01:51 -0500

    working on the play space
    continued work of PuzzleSection

--- PuzzleSection.java
+++ PuzzleSection.java
@@ -1,17 +1,37 @@
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.awt.event.MouseEvent;
 
 public class PuzzleSection {
 
-    private TableView<GameCell> section;
+    private TableView<TableRowData> section;
+    private int numberOfColumns;
 
     public PublicSection(){
+
         this.section = new TableView<>();
-    }
-
-    public void setHeader(String header){
+        this.numberOfColumns = 0;
 
     }
 
-    public void
+    public void setRowHeader(String header){
+        String[] pieces = header.split(",");
+        TableColumn<TableRowData, String> columnHeader = new TableColumn<>("Header");
+        for(String next: pieces){
+            columnHeader.setCellFactory(cellData -> cellData.getValue().getFirstColumn());
+        }
+        this.numberOfColumns = this.numberOfColumns + 1;
+    }
+    public void setColumn(String header){
+        TableColumn<Table, String> nextColumn = TableColumn<>(header);
+
+        this.section.getColumns().add(nextColumn);
+    }
+
+        //Used to add Click events to Table Elements
+    public void addClickEvent(MouseEvent event){
+
+    }
 
 }
