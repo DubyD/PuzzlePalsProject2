e00f08712371a5bb3e03f5f029db536fe022dfb4
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-02-23 21:25:21 -0500

    added section parsing

--- PuzzleReader.java
+++ PuzzleReader.java
@@ -9,6 +9,17 @@ public class PuzzleReader{
     private static String labelDataFilePath="./csv/labelHeading.csv";
     private static File csvFile=new File(csvFilePath);
     private static File labelData = new File(labelDataFilePath);
+    private static String path3x4 = "./csv/3x4answerKey.csv";
+    private static String path4x4 = "./csv/4x4answerKey.csv";
+    private static String path4x5 = "./csv/4x5answerKey.csv";
+
+    private static File file3x4=new File(path3x4);
+    private static File file4x4=new File(path4x4);
+    private static File file4x5=new File(path4x5);
+
+
+
+
     public static String readCSV(String puzInput){
         //puzInput i.e = "3x4"
         String categories; String items_in_categories; 
@@ -18,6 +29,7 @@ public class PuzzleReader{
         ArrayList<ArrayList<String>> topData = new ArrayList<>();
         ArrayList<ArrayList<String>> leftData = new ArrayList<>();
         ArrayList<String> clues = new ArrayList<String>();
+        ArrayList<String[][]> puzzleSections = new ArrayList<>();
         try{
             Scanner csvScanner=new Scanner(csvFile);
             String[] inputCoordinates=puzInput.split("x");
@@ -90,16 +102,59 @@ public class PuzzleReader{
                             }
                         }
                     }
-
                 }
             }
-            System.out.println(topHeaders);
+            Scanner sectionScanner=null;
+            if(inputCoordinates[0].equals("3")&&inputCoordinates[1].equals("4")){
+                sectionScanner=new Scanner(file3x4);
+            }
+            if(inputCoordinates[0].equals("4")&&inputCoordinates[1].equals("4")){
+                sectionScanner = new Scanner(file4x4);
+            }
+            if(inputCoordinates[0].equals("4")&&inputCoordinates[1].equals("5")){
+                sectionScanner = new Scanner(file4x5);
+            }
+
+            String[][] currentSection = new String[Integer.parseInt(inputCoordinates[1])][Integer.parseInt(inputCoordinates[1])];
+            int parseCounter=0;
+            while(sectionScanner.hasNext()){
+                String currentLine = sectionScanner.nextLine();
+                if(currentLine.equals("")){
+                    puzzleSections.add(currentSection);
+                    System.out.println("reached empty.");
+                    parseCounter=0;
+                    currentSection=new String[Integer.parseInt(inputCoordinates[1])][Integer.parseInt(inputCoordinates[1])]; // clear the array.
+                    continue;
+                }
+                String[] currentLineParsed = currentLine.split(",");
+                //^^ => i.e [X,X,O,X,X];
+                /*{1, 2, 3}, [[0,0],[0,1],0,2]
+                  {4, 5, 6}, [[1,0],[1,1],[1,2]]
+                  {7, 8, 9} */ //[2,0],[2,1],[2,2]
+                for(int i=0;i<currentLineParsed.length;i++){
+                    currentSection[parseCounter][i]=currentLineParsed[i];
+                }
+                parseCounter++;
+            }
+            puzzleSections.add(currentSection); // add after EOF :)
+
+              //Print all sections {testing}
+            for(String[][] x: puzzleSections){
+                for(int i=0;i<x.length;i++){
+                    System.out.println(x[i][0]+x[i][1]+x[i][2]+x[i][3]+x[i][4]+"\n");
+                }
+            }
+
+
+
+
+            /*System.out.println(topHeaders);
             System.out.println(topData);
             System.out.println("\n");
             System.out.println(leftHeaders);
             System.out.println(leftData);
             System.out.println("\n");
-            System.out.println(clues);
+            System.out.println(clues);*/
             // make the first label null/empty for empty cell parsing. - wd
         }catch(FileNotFoundException e){
             System.out.println("File not found.");
