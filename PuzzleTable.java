b2245a4744bd1882639983dc388db97f2daeb36e
DubyD <140568071+DubyD@users.noreply.github.com>
2024-02-24 19:18:40 -0500

    missing ;

--- PuzzleTable.java
+++ PuzzleTable.java
@@ -181,7 +181,7 @@ public class PuzzleTable {
                 }
             }
         }
-        return reply
+        return reply;
     }
 
     public String getHint(){
@@ -190,7 +190,7 @@ public class PuzzleTable {
         for(int x = 0; x < this.sizeLength; x++) {
             for (int y = 0; y < this.sizeWidth; y++) {
                 if(puzzlePiece[x][y].isError() == false){
-                    reply = puzzlePiece[x][y].getX() + "x" puzzlePiece[x][y].getY();
+                    reply = puzzlePiece[x][y].getX() + "x" + puzzlePiece[x][y].getY();
                     reply = reply + " is supposed to be " + puzzlePiece[x][y].getAnswerKey();
                     return reply;
                 }
